db1a10b6d780fff7ad0afcaa6ccd50c78d3a4d16
Tsutomu001 <tsutomuadachi8@gmail.com>
2022-10-21 21:50:29 +0900

    Middleware設定

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,8 +4,15 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
+// Routeファサードを使用するため定義する
+use Illuminate\Support\Facades\Route;
+
 class Authenticate extends Middleware
 {
+    // 変数をそれぞれのログイン画面として定義する
+    protected $user_route = 'user.login';
+    protected $owner_route = 'owner.login';
+    protected $admin_route = 'admin.login';
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -14,8 +21,17 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        // expectsJsonメソッド...、受信リクエストがJSONリクエストを期待しているかを判定する
+        // つまり以下の処理は「ログインユーザーではなかったら....」という意味
         if (! $request->expectsJson()) {
-            return route('login');
+            // isメゾット...○○○関連のルート
+            if(Route::is('owner.*')){
+                return route($this->owner_route);
+            } elseif(Route::is('admin.*')){
+                return route($this->admin_route);
+            } else {
+                return route($this->user_route);
+            }
         }
     }
 }
