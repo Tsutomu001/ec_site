048ab9e58cb8b2a3d53f31ed74ef1f7fc0ba73e3
Tsutomu001 <tsutomuadachi8@gmail.com>
2022-10-26 15:01:02 +0900

    フラッシュメッセージ作成

---
+++ resources/views/components/flash-message.blade.php
@@ -0,0 +1,17 @@
+{{-- @props ...コンポーネントをした際に、値が空だった場合に表示する文字を設定 --}}
+@props(['status' => 'info'])
+
+{{-- 背景について --}}
+@php
+// 正しい情報の場合
+if($status === 'info'){$bgColor = 'bg-blue-300';}
+// 正しくない情報の場合
+if($status === 'error'){$bgColor = 'bg-red-500';}
+@endphp
+
+{{-- メッセージを取得した場合 --}}
+@if(session('message'))
+    <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white">
+        {{ session('message')}}
+    </div>
+@endif
