ea0ac4612626aa78980e9225a757486183742de5
Tsutomu001 <tsutomuadachi8@gmail.com>
2022-10-28 15:21:49 +0900

    外部キー制約・Shopシーダーデータ

---
+++ database/migrations/2022_10_28_145011_create_shops_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            // foreignIdメソッド ...UNSIGNED BIGINTカラムを作成する
+            // constrainedメソッド ...規約を使用して参照されるテーブルとカラムの名前を決定する
+            $table->foreignId('owner_id')->constrained();
+            $table->string('name'); 
+            $table->text('information'); 
+            $table->string('filename');
+            // 販売中or販売中止
+            $table->boolean('is_selling');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
