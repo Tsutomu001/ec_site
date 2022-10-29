7a6feb12501cb70586be5ce6d1f2aa1d11b4eabd
Tsutomu001 <tsutomuadachi8@gmail.com>
2022-10-29 13:34:08 +0900

    Shopのミドルウェア設定

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -13,6 +13,21 @@ class ShopController extends Controller
     public function __construct()
     {
         $this->middleware('auth:owners');
+
+        // 単一コントローラでmiddlewareを定義する
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('shop'); //shopのid取得(文字列) 
+            if(!is_null($id)){ // null判定 
+                    $shopsOwnerId = Shop::findOrFail($id)->owner->id; //shopのidからownerのidを取得する
+                    $shopId = (int)$shopsOwnerId; // キャスト ...(int)文字列→数値に型変換 
+                    $ownerId = Auth::id();// ログインしたownerのidを取得する
+                if($shopId !== $ownerId){ // 同じでなかったら 
+                    abort(404); // 404画面表示 
+                    } 
+            } 
+            return $next($request); 
+        });
     }
 
     public function index()
@@ -25,6 +40,7 @@ class ShopController extends Controller
 
     public function edit($id)
     {
+        dd(Shop::findOrFail($id));
     }
 
     public function update(Request $request, $id)
