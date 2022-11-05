f1091bb0a8065994060ef9dc8a0e97d9d56bde57
Tsutomu001 <tsutomuadachi8@gmail.com>
2022-11-05 17:10:15 +0900

    ユーザーのダミーデータ作成

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+// DBファサードとHashファサードを使用するため定義する
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // insertメゾット ...データを登録することができる
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'password' => Hash::make('password123'),
+            'created_at' => '2021/01/01 11:11:11'
+
+        ]);
+    }
+}
