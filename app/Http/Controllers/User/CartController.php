ba30a36e0f17f8ccd703cd246b5029bca24ca463
Tsutomu001 <tsutomuadachi8@gmail.com>
2022-11-07 22:46:05 +0900

    カートに入れる(保存処理)

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -4,11 +4,31 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+// Auth定義
+use Illuminate\Support\Facades\Auth;
+// Cartモデル定義
+use App\Models\Cart;
 
 class CartController extends Controller
 {
     public function add(Request $request)
     {
-        dd($request);
+        // 'product_id'と'user_id'が正しいかどうか？
+        $itemInCart = Cart::where('product_id' , $request->product_id)
+            ->where('user_id' , Auth::id())->first();
+
+        if($itemInCart){
+            // カートに入れた数を加算していく
+            $itemInCart->quantity += $request->quantity;
+            $itemInCart->save();
+
+        } else {
+            Cart::create([
+                'user_id' => Auth::id(),
+                'product_id' => $request->product_id,
+                'quantity' => $request->quantity
+            ]);
+        }
+        dd('テスト');
     }
 }
