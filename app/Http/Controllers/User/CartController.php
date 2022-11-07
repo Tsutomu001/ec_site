c8164ce1ab87261e69a46abc0ee601975fabce10
Tsutomu001 <tsutomuadachi8@gmail.com>
2022-11-08 00:33:45 +0900

    Cartのindexメゾット処理

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -8,9 +8,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 // Cartモデル定義
 use App\Models\Cart;
+// Userモデル定義
+use App\Models\User;
 
 class CartController extends Controller
 {
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice = 0;
+
+        foreach($products as $product){
+            $totalPrice += $product->price * $product->pivot->quantity;
+        }
+
+        dd($products,$totalPrice);
+
+        return view('user.cart.index',
+            compact('products', 'totalPrice'));
+    }
+
     public function add(Request $request)
     {
         // 'product_id'と'user_id'が正しいかどうか？
@@ -29,6 +47,7 @@ class CartController extends Controller
                 'quantity' => $request->quantity
             ]);
         }
-        dd('テスト');
+        
+        return redirect()->route('user.cart.index');
     }
 }
